50718375390d3bd9c185d03d30e912346d6fb0cb
Richard Middleditch <richard.middleditch1@bbc.co.uk>
2014-02-21 09:33:54 +0000

    Adding log routes

---
+++ scripts/pal.js
@@ -0,0 +1,14 @@
+// Description:
+//   Commands to alert people in the room
+// Commands:
+//   hubot logs for (.*)(int|test|stage|live)
+module.exports = function(robot) {
+    var logAddress = "https://logs.forge.bbc.co.uk/tail/tail/{{env}}/service-pal-{{env}}-app-logs/pal/bbc.co.uk/{{project}}/error_log?lines=100";
+    robot.respond(/logs for (.*)(int|test|stage|live)/i, function (msg) {
+        var project = msg.match[1].replace(/[a-z0-9]/i, '') || 'tviplayer',
+            environment = msg.match[2],
+            log = logAddress.replace('{{env}}', environment).replace('{{project}}', project);
+
+        msg.send('Here you go ' + log);
+    });
+}
